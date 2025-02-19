193e0d9f676d41f1263a8a816ccf3f7030b2fe34
Vic Lo <vic.lo@shopline.com>
2025-02-19 11:08:47 +0800

    feat: add smell function

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"error_handling/utils"
 	"errors"
 	"fmt"
-
-	"error_handling/utils"
 )
 
 var (
@@ -22,6 +21,16 @@ func eat(food_type, amount int) (bool, error) {
 	return true, nil
 }
 
+func smell(food_type, amount int) (bool, error) {
+	if food_type != 0 {
+		return false, ErrorFoodType
+	} else if amount > 5 {
+		return false, ErrorFoodAmount
+	}
+	fmt.Println("good to smell !!")
+	return true, nil
+}
+
 func main() {
 	defer func() {
 		if err := recover(); err != nil {
@@ -32,7 +41,14 @@ func main() {
 			}
 		}
 	}()
+	for i := range 5 {
+		ok, err := smell(i, 0)
+		if !ok {
+			fmt.Printf("smell not good, but it's ok: %s\n", err)
+		}
+	}
+
 	utils.Must(eat(0, 1))
-	// utils.Must(eat(1, 1))
-	utils.Must(eat(0, 20))
+	utils.Must(eat(1, 1))
+	utils.Must(eat(0, 6))
 }
